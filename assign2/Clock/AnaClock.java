be75ee7ec6d758f35476881aad7feaaaf152f25c
Rohit Das <rohit.das950@gmail.com>
2017-10-08 14:36:09 +0530

    AnaClock.java: Added digits on clock
    Signed-off-by: Rohit Das <rohit.das950@gmail.com>

--- assign2/Clock/AnaClock.java
+++ assign2/Clock/AnaClock.java
@@ -42,7 +42,7 @@ public class AnaClock extends Applet implements Runnable
 	public void run () {
 		try {
 			while (true) {
-				Calendar cal = Calendar.getInstance();	
+				Calendar cal = Calendar.getInstance();
 
 				h = cal.get( Calendar.HOUR_OF_DAY );
 	            if ( h > 12 ) h -= 12;
@@ -68,34 +68,38 @@ public class AnaClock extends Applet implements Runnable
 		catch (Exception e) { }
 	}
 
-	void drawHand( double angle, int radius, Graphics g ) {  
-      angle -= 0.5 * Math.PI;  
-      int x = (int)( radius*Math.cos(angle) );  
-      int y = (int)( radius*Math.sin(angle) );  
-      g.drawLine( width/2, height/2, width/2 + x, height/2 + y );  
-   	}  
-  
-   	void drawWedge( double angle, int radius, Graphics g ) {  
-      angle -= 0.5 * Math.PI;  
-      int x = (int)( radius*Math.cos(angle) );  
-      int y = (int)( radius*Math.sin(angle) );  
-      angle += 2*Math.PI/3;  
-      int x2 = (int)( 5*Math.cos(angle) );  
-      int y2 = (int)( 5*Math.sin(angle) );  
-      angle += 2*Math.PI/3;  
-      int x3 = (int)( 5*Math.cos(angle) );  
-      int y3 = (int)( 5*Math.sin(angle) );  
-      g.drawLine( width/2+x2, height/2+y2, width/2 + x, height/2 + y );  
-      g.drawLine( width/2+x3, height/2+y3, width/2 + x, height/2 + y );  
-      g.drawLine( width/2+x2, height/2+y2, width/2 + x3, height/2 + y3 );  
-   	}  
-  
-   	public void paint( Graphics g ) {  
-      g.setColor( Color.gray );  
-      drawWedge( 2*Math.PI * h / 12, width/5, g );  
-      drawWedge( 2*Math.PI * min / 60, width/3, g );  
-      drawHand( 2*Math.PI * s / 60, width/2, g );  
-      g.setColor( Color.white );  
-      g.drawString( time, 10, height-10 );  
-   	}  
+	void drawHand( double angle, int radius, Graphics g ) {
+      angle -= 0.5 * Math.PI;
+      int x = (int)( radius*Math.cos(angle) );
+      int y = (int)( radius*Math.sin(angle) );
+      g.drawLine( width/2, height/2, width/2 + x, height/2 + y );
+   	}
+
+   	void drawWedge( double angle, int radius, Graphics g ) {
+      angle -= 0.5 * Math.PI;
+      int x = (int)( radius*Math.cos(angle) );
+      int y = (int)( radius*Math.sin(angle) );
+      angle += 2*Math.PI/3;
+      int x2 = (int)( 5*Math.cos(angle) );
+      int y2 = (int)( 5*Math.sin(angle) );
+      angle += 2*Math.PI/3;
+      int x3 = (int)( 5*Math.cos(angle) );
+      int y3 = (int)( 5*Math.sin(angle) );
+      g.drawLine( width/2+x2, height/2+y2, width/2 + x, height/2 + y );
+      g.drawLine( width/2+x3, height/2+y3, width/2 + x, height/2 + y );
+      g.drawLine( width/2+x2, height/2+y2, width/2 + x3, height/2 + y3 );
+   	}
+
+   	public void paint( Graphics g ) {
+      g.setColor( Color.gray );
+      drawWedge( 2*Math.PI * h / 12, width/5, g );
+      drawWedge( 2*Math.PI * min / 60, width/3, g );
+      drawHand( 2*Math.PI * s / 60, width/2, g );
+      g.setColor( Color.white );
+      g.drawString( time, 10, height-10 );
+      g.drawString("12", width / 2, 12);
+      g.drawString("6", width / 2, height - 12);
+      g.drawString("9", 10, height / 2);
+      g.drawString("3", width - 10, height / 2);
+   	}
 }
