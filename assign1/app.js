c18df03cc030217a6c9d183c4a7815340adb6d4f
Rohit Das <rohit.das950@gmail.com>
2017-11-24 18:23:54 +0530

    Minor Changes
    Signed-off-by: Rohit Das <rohit.das950@gmail.com>

--- assign1/app.js
+++ assign1/app.js
@@ -1,31 +1,22 @@
+/*
+* app.js
+*/
 function changeColor() {
 	var e = document.getElementById('t-color-inp');
 	var color = e.options[e.selectedIndex].value;
 	var colour = "#d37070"
 	switch(color) {
 		case 'red':
-			colour = "#d37070";
-			break;
-
+			colour = "#d37070";	break;
 		case 'green':
-			colour = "#51db5a";
-			break;
-			
+			colour = "#51db5a";	break;		
 		case 'yellow':
-			colour = "#edd30e";
-			break;
-
+			colour = "#edd30e";	break;
 		case 'black':
-			colour = "#000000";
-			break;
-
+			colour = "#000000";	break;
 		case 'white':
-			colour = "#ffffff";
-			break;
-
+			colour = "#ffffff";	break;
 		default:
 			colour = "#ffffff";
-	}
-
-	document.getElementById('container').style.backgroundColor = colour
+	}	document.getElementById('container').style.backgroundColor = colour
 }
